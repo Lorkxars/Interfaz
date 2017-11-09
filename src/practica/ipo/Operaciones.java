c2f938b2726e42c6d3efb266b0c882590c55f3da
pablo <pablo@DESKTOP-JTIREQD>
2017-11-09 18:01:51 +0100

    Operaciones cambiadas

--- src/practica/ipo/Operaciones.java
+++ src/practica/ipo/Operaciones.java
@@ -36,30 +36,45 @@ public class Operaciones extends javax.swing.JFrame {
         jButton7 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(1680, 1050));
 
         jPanel3.setMinimumSize(new java.awt.Dimension(1920, 1080));
         jPanel3.setLayout(null);
 
         jLabel5.setFont(new java.awt.Font("Tahoma", 0, 72)); // NOI18N
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel5.setText("¿Qué quieres hacer hoy?");
         jPanel3.add(jLabel5);
-        jLabel5.setBounds(160, 40, 800, 130);
+        jLabel5.setBounds(0, 0, 1680, 130);
 
+        jButton3.setBackground(new java.awt.Color(255, 133, 255));
+        jButton3.setFont(new java.awt.Font("Tahoma", 0, 100)); // NOI18N
         jButton3.setText("División");
         jPanel3.add(jButton3);
-        jButton3.setBounds(820, 240, 210, 130);
+        jButton3.setBounds(830, 570, 830, 440);
 
+        jButton4.setBackground(new java.awt.Color(145, 206, 189));
+        jButton4.setFont(new java.awt.Font("Tahoma", 0, 100)); // NOI18N
         jButton4.setText("Suma ");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
         jPanel3.add(jButton4);
-        jButton4.setBounds(140, 240, 210, 130);
+        jButton4.setBounds(0, 130, 830, 440);
 
+        jButton5.setBackground(new java.awt.Color(253, 223, 107));
+        jButton5.setFont(new java.awt.Font("Tahoma", 0, 100)); // NOI18N
         jButton5.setText("Resta");
         jPanel3.add(jButton5);
-        jButton5.setBounds(360, 240, 210, 130);
+        jButton5.setBounds(830, 130, 830, 440);
 
+        jButton6.setBackground(new java.awt.Color(139, 226, 255));
+        jButton6.setFont(new java.awt.Font("Tahoma", 0, 100)); // NOI18N
         jButton6.setText("Multiplicación");
         jPanel3.add(jButton6);
-        jButton6.setBounds(590, 240, 210, 130);
+        jButton6.setBounds(0, 570, 830, 440);
 
         jButton7.setFont(new java.awt.Font("Tahoma", 0, 72)); // NOI18N
         jButton7.setText("Tira");
@@ -70,25 +85,29 @@ public class Operaciones extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1170, Short.MAX_VALUE)
+            .addGap(0, 1680, Short.MAX_VALUE)
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(layout.createSequentialGroup()
                     .addGap(0, 0, Short.MAX_VALUE)
-                    .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, 1170, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, 1680, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGap(0, 0, Short.MAX_VALUE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 630, Short.MAX_VALUE)
+            .addGap(0, 1050, Short.MAX_VALUE)
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(layout.createSequentialGroup()
                     .addGap(0, 0, Short.MAX_VALUE)
-                    .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, 630, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, 1050, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGap(0, 0, Short.MAX_VALUE)))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     /**
      * @param args the command line arguments
